235d575feb936b89ee985c7c522af5ed0769fd63
pranikz <18btcse42@suiit.ac.in>
2023-08-04 22:01:37 +0530

    fix: baseUrl fixes

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -7,59 +7,16 @@ import {
   Line,
   XAxis,
   YAxis,
-  CartesianGrid,
   Tooltip,
   Legend,
   ResponsiveContainer,
 } from "recharts";
+
 type Props = {};
 
-const chartdata = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 const baseUrl = "https://gumlethq-assignment.vercel.app/api/hello";
+
+//use this url for local http://localhost:3000/api/hello
 
 async function fetchData(
   page: number,
@@ -81,11 +38,10 @@ async function fetchData(
     throw error;
   }
 }
+
 const page = 1;
 const limit = 40;
 const period = 380;
-
-// Call the fetchData function with the specified parameters
 
 const Dashboard = (props: Props) => {
   const [data, setData] = useState<Video>();
@@ -149,7 +105,7 @@ const Dashboard = (props: Props) => {
       </div>
 
       {/* chart data */}
-      <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 w-[98%] mx-auto ">
+      <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 w-[98%] mx-auto mb-20 ">
         <div className=" dark:bg-[#1b1b1b] bg-gray-200  p-4 rounded-md ">
           <h2 className="text-purple-500 dark:text-purple-300  text-lg font-semibold pb-4">
             Bandwidth Usage
